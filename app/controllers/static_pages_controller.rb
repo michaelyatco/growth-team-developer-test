4c62c17f95cb34ce4a45cda4012694e6626ea010
michaelyatco <mjyatco@gmail.com>
2017-06-15 16:36:16 -0400

    Aligning and sorting root page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
 class StaticPagesController < ApplicationController
 
   def root
-  	search_result = params[:search_result]
-  	@gems = Unirest.get("https://rubygems.org/api/v1/search.json?query=#{search_result}").body
+  	 search_result = params[:search_result]
+  	 @gems = Unirest.get("https://rubygems.org/api/v1/search.json?query=#{search_result}").body
+  	 if search_result == nil
+  	   flash[:warning] = "Oh no! Looks like this gem cannot be found!"
+  	 end
   end
 
   def favorites
