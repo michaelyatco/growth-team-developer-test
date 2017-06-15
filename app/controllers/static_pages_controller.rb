5cb800744f967f5d1adf16d18eb7031da52a8814
michaelyatco <mjyatco@gmail.com>
2017-06-15 17:44:19 -0400

    css alterations for root page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,13 +2,15 @@ class StaticPagesController < ApplicationController
 
   def root
   	 search_result = params[:search_result]
-  	 @gems = Unirest.get("https://rubygems.org/api/v1/search.json?query=#{search_result}").body
-  	 if search_result == nil
-  	   flash[:warning] = "Oh no! Looks like this gem cannot be found!"
-  	 end
+  	 if search_result
+	  @gems = Unirest.get("https://rubygems.org/api/v1/search.json?query=#{search_result}").body
+  	else
+  	  @gems = []
+  	end
   end
 
   def favorites
+  	@favorites = gems.favorites.create
   end
 
 end
