97b35fef539b2b34c7045b90961917b4c62dd5de
michaelyatco <mjyatco@gmail.com>
2017-06-16 16:18:34 -0400

    added images to root page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,16 @@
 class StaticPagesController < ApplicationController
 
   def root
-  	 search_result = params[:search_result]
-  	 if search_result
-	  @gems = Unirest.get("https://rubygems.org/api/v1/search.json?query=#{search_result}").body
+  	search_result = params[:search_result]
+  	if search_result
+	    @gems = Unirest.get("https://rubygems.org/api/v1/search.json?query=#{search_result}").body
   	else
   	  @gems = []
   	end
   end
 
   def favorites
-  	@favorites = gems.favorites.create
+    # @favorites = gems.favorites.create
   end
 
 end
