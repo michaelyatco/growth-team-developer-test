e6ba1bfa452a7826fba7e16e5b925e772e7d495d
michaelyatco <mjyatco@gmail.com>
2017-06-18 13:29:18 -0400

    Clicking star adds/removes gem from localStorage while changing to appropriate color

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -1,10 +1,15 @@
 function addToListFavGems(star){
-  var favGems = [];
+  if (localStorage.getItem('gemNameKey')) {
+    var favGems = JSON.parse(localStorage.getItem('gemNameKey'));
+  } else {
+    var favGems = [];
+  }
   gemName = star.previousSibling.parentNode.innerText;
-  if (favGems.indexOf(gemName) === -1) { // check if gem is in local storage
+  if (favGems.indexOf(gemName) === -1) {
     favGems.push(gemName);
   } else {
-    removeGemNames();
+    console.log("time to remove!");
+    removeGemNames(favGems, gemName);
   }
   if(window.localStorage){
     localStorage.setItem('gemNameKey', JSON.stringify(favGems))
@@ -16,9 +21,16 @@ function addToListFavGems(star){
   }
 }
 
-function removeGemNames(){
+function removeGemNames(favGems, gemName) {
   if (window.localStorage) {
-    var gemNames = localStorage.removeItem('gemNameKey');
+    console.log("I'm removing the gem")
+    if (favGems.indexOf(gemName) !== -1) {
+      console.log(favGems);
+      favGems.splice(favGems.indexOf(gemName), 1);
+      console.log(favGems);
+      localStorage.removeItem('gemNameKey');
+      localStorage.setItem('gemNameKey', JSON.stringify(favGems));
+    }
   }
 }
 
