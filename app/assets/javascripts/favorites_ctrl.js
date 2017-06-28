081e716f9cd9d08f4ef1b13caf2c37692d426e44
michaelyatco <mjyatco@gmail.com>
2017-06-28 15:28:32 -0400

    resolved angular issue

--- app/assets/javascripts/favorites_ctrl.js
+++ app/assets/javascripts/favorites_ctrl.js
@@ -2,7 +2,7 @@
 (function() {
   "use strict";
 
-  angular.module("app").controller("favoritesCtrl", function($scope, $http, $window) {
+  angular.module("app", []).controller("favoritesCtrl", function($scope, $http, $window) {
     $scope.gemArray = JSON.parse($window.localStorage.getItem('gemNameKey'));
     $scope.remove = function($index) {
       $scope.gemArray.splice($index,1);
